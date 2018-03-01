92c80b92c9b5dd89f8c56948340abb9d769a3850
Vasil Nikolov <phpnikolov@gmail.com>
2018-03-01 03:25:57 -0800

    Updated method fightCreature, less gas used, code improvements

--- contracts/crypto-battles.sol
+++ contracts/crypto-battles.sol
@@ -15,23 +15,23 @@ contract Ownable {
 
 contract Random {
 
-    uint private seed ;
+    uint private lastRandom ;
     
-    function generateSeed() internal returns (uint) {
-        seed = uint256(keccak256(
-            seed,
+    function random() internal returns (uint) {
+        lastRandom = uint256(keccak256(
+            lastRandom,
             block.blockhash(block.number - 1),
             block.coinbase,
             block.difficulty
         ));
-    return seed;
+    return lastRandom;
   }
 
 
-  function random(uint _seed, uint _min, uint _max) internal pure returns (uint) {
+  function rangedRandom(uint _random, uint _min, uint _max) internal pure returns (uint) {
       require(_min < _max);
 
-    uint r = _seed % (_max - _min + 1);
+    uint r = _random % (_max - _min + 1);
     return _min + r;
   }
   
@@ -41,7 +41,7 @@ contract Random {
 contract CryptoBattles is Ownable, Random {
     
     enum CreatureType {
-        unknown,
+        dead,
         Halfling,
         Rogue,
         Nomad
@@ -57,11 +57,9 @@ contract CryptoBattles is Ownable, Random {
     }
     
     struct Creature {
-        uint damageMin;
-        uint damageMax;
+        uint damage;
         uint health;
-        uint diamondsMin;
-        uint diamondsMax;
+        uint gold;
         uint experience;
     }
     
@@ -69,7 +67,8 @@ contract CryptoBattles is Ownable, Random {
         bytes12 username;
         uint registrationBlock;
         uint lastSynced; // block number
-        uint diamonds;
+        uint level;
+        uint gold;
         uint experience;
         uint health;
         
@@ -79,6 +78,8 @@ contract CryptoBattles is Ownable, Random {
         
         // when was killed idx => day
         mapping (uint8 => uint) killedCreatures;
+        
+        uint deadUntil; // block number
     }
 
     // all registered players
@@ -88,34 +89,32 @@ contract CryptoBattles is Ownable, Random {
     mapping (uint8 => Creature) private creatures;
     
     function CryptoBattles() public {
-        // Halfling (10)
+        // base formula
+        // gold = (damage + health) * 2
+        // experience = (damage + health) * 5
+        
+        // Halfling
         creatures[uint8(CreatureType.Halfling)] = Creature({
-            damageMin: 1,
-            damageMax: 3,
-            health: 4,
-            diamondsMin: 8,
-            diamondsMax: 12,
-            experience: 30
+            damage: 3,
+            health: 5,
+            gold: 16, // 8 * 2
+            experience: 40 // 8 * 5
         });
         
-        // Rogue (15)
+        // Rogue
         creatures[uint8(CreatureType.Rogue)] = Creature({
-            damageMin: 2,
-            damageMax: 4,
-            health: 6,
-            diamondsMin: 15,
-            diamondsMax: 25,
-            experience: 40
+            damage: 7,
+            health: 5,
+            gold: 36, // 12 * 3 (50% more gold)
+            experience: 40 // 12 * 3.3 (50% less exp)
         });
         
-        // Nomad (23)
+        // Nomad
         creatures[uint8(CreatureType.Nomad)] = Creature({
-            damageMin: 2,
-            damageMax: 6,
-            health: 11,
-            diamondsMin: 20,
-            diamondsMax: 24,
-            experience: 80
+            damage: 8,
+            health: 16,
+            gold: 32, // 24 * 1.3 (50% less gold)
+            experience: 180 // 24 * 7.5 (50% more exp)
         });
     }
     
@@ -130,50 +129,41 @@ contract CryptoBattles is Ownable, Random {
 
     function register(bytes12 _username) public {
         require(isRegistered(msg.sender) == false);
-        require(_username.length >= 4);
 
         players[msg.sender] = Player({
             username: _username,
             registrationBlock: block.number,
             lastSynced: block.number,
-            diamonds: random(generateSeed(), 900, 1100),
+            level: 1,
+            gold: 1000,
             experience: 0,
             health : 100,
             
             strengthPoints:0,
             vitalityPoints:0,
-            intelligencePoints:0
-            
+            intelligencePoints:0,
+            deadUntil: 0
         });
     }
     
+    // day length 2400 blocks (~10 hours)
     function getDay(address _addr) private view returns(uint) {
-        return 1 + (block.number - players[_addr].registrationBlock) / 1000;
+        return 1 + (block.number - players[_addr].registrationBlock) / 2400;
+    }
+    
+    function isDead(address _addr) private view returns(bool) {
+        return players[_addr].deadUntil > block.number;
     }
     
     // How many experience is needed for a certain level http://www.wolframalpha.com/input/?i=40*(x+-+1)(x+%2B+8);+x+from+1+to+99
     function getLevelSize(uint _level) private pure returns(uint) {
-        require(_level > 0);
         return (40 * (_level - 1) * (_level + 8));
     }
     
-    function getLevel(address _addr) private view returns(uint) {
-
-        for (uint i = 0; i < 99; i++) {
-            if (players[_addr].experience < getLevelSize(i + 1)) {
-                return i;
-            }
+    function syncLevel(address _addr) private {
+        while (players[_addr].experience > getLevelSize(players[_addr].level + 1)) {
+            players[_addr].level++;
         }
-        return 99;
-    }
-    
-    function getUnsyncedBlocks(address _addr) private view returns(uint) {
-        return block.number - players[_addr].lastSynced;
-    }
-    
-    function syncPlayer(address _addr) private {
-        players[_addr].health = getHealth(_addr);
-        players[_addr].lastSynced = block.number;
     }
     
     function getStrength(address _addr) private view returns(uint) {
@@ -188,8 +178,10 @@ contract CryptoBattles is Ownable, Random {
         return 20 + players[_addr].intelligencePoints;
     }
     
-    function getHealthPerBlock(address _addr) private view returns(uint) {
-        return (1 * getIntelligence(_addr));
+    function getHealthPer100Block(address _addr) private view returns(uint) {
+        // 1 intelligence = 0.1 health per block (10 health per 100block)
+        
+        return (10 * getIntelligence(_addr));
     }
     
     function getMaxHealth(address _addr) private view returns(uint) {
@@ -197,184 +189,190 @@ contract CryptoBattles is Ownable, Random {
     }
     
     function getHealth(address _addr) private view returns(uint) {
-        uint notSyncedHealth = getUnsyncedBlocks(_addr) * getHealthPerBlock(_addr);
+        if (isDead(_addr)) {
+            return 0;
+        }
+        uint notSyncedBlocks = (block.number - players[_addr].lastSynced);
+        uint notSyncedHealth =  notSyncedBlocks * getHealthPer100Block(_addr) / 100;
         
         if (players[_addr].health + notSyncedHealth > getMaxHealth(_addr)) {
             return getMaxHealth(_addr);
         }
         
-        return  players[_addr].health + notSyncedHealth;
+        return players[_addr].health + notSyncedHealth;
     }
     
     function getDamage(address _addr) private view returns(uint) {
-        return getStrength(_addr) * 1;
+        return getStrength(_addr);
     }
-    
-    // convert CreatureCount to random value in specified intervals
-    function getCreaturesCount(CreatureCount cc) private returns(uint) {
-        if (cc == CreatureCount.Few) {
-            return random(generateSeed(), 1, 4);
+
+    function creatureCount2Number(CreatureCount _creatureCount) private returns(uint) {
+        if (_creatureCount == CreatureCount.Few) {
+            return rangedRandom(random(), 1, 4);
         }
         
-        if (cc == CreatureCount.Several) {
-            return random(generateSeed(), 5, 9);
+        if (_creatureCount == CreatureCount.Several) {
+            return rangedRandom(random(), 5, 9);
         }
         
-        if (cc == CreatureCount.Pack) {
-            return random(generateSeed(), 10, 19);
+        if (_creatureCount == CreatureCount.Pack) {
+            return rangedRandom(random(), 10, 19);
         }
         
-        if (cc == CreatureCount.Lots) {
-            return random(generateSeed(), 20, 49);
+        if (_creatureCount == CreatureCount.Lots) {
+            return rangedRandom(random(), 20, 49);
         }
         
-        if (cc == CreatureCount.Horde) {
-            return random(generateSeed(), 50, 99);
+        if (_creatureCount == CreatureCount.Horde) {
+            return rangedRandom(random(), 50, 99);
         }
         
         return 0;
     }
     
-    
-    // pseudo random Creature which is unique for the day (1000 blocks)
-    // CreatureType = _creatureCode % 100;
-    // CreatureCount =  (_creatureCode - CreatureType) / 100
-    function getCreature(address _addr, uint8 _creatureIdx) private view returns(uint _creatureCode){
+
+    function getCreature(address _addr, uint day, uint8 _creatureIdx) private view returns(CreatureType _type, CreatureCount _count){
         require(_creatureIdx < 6);
         
-        uint lvl = getLevel(_addr);
-        
-        uint staticSeed1 = uint256(keccak256(_creatureIdx, getDay(_addr), _addr));
-        uint rand1 = random(staticSeed1, 0, 99);
-        
-        CreatureType creature;
-        CreatureCount count;
+        if ( players[_addr].killedCreatures[_creatureIdx] >= day) {
+            _type = CreatureType.dead; // this creature is dead
+            _count = CreatureCount.unknown;
+            return;
+        }
 
-        if (lvl <= 5) {
+        uint staticRand = uint256(keccak256(_creatureIdx, day, _addr));
+        uint rand = rangedRandom(staticRand, 0, 99);
+
+        if (players[_addr].level <= 5) {
             /* <= Level 5
-             * Halfling - Several (25%)
-             * Halfling - Few (15%)
+             * Halfling - Several (30%)
+             * Rogue - Several (30%)
+             * Nomad - Few (30%)
              * Halfling - Pack (10%)
-             * Rogue - Few (20%)
-             * Rogue - Several (20%)
-             * Nomad - Few (10%)
              */
-            if (rand1 < 25) {
-                creature = CreatureType.Halfling;
-                count = CreatureCount.Several;
+            if (rand < 30) {
+                _type = CreatureType.Halfling;
+                _count = CreatureCount.Several;
             }
-            else if (rand1 < 40) {
-                creature = CreatureType.Halfling;
-                count = CreatureCount.Few;
+            else if (rand < 60) {
+                _type = CreatureType.Rogue;
+                _count = CreatureCount.Several;
             }
-            else if (rand1 < 50) {
-                creature = CreatureType.Halfling;
-                count = CreatureCount.Pack;
-            }
-            else if (rand1 < 70) {
-                creature = CreatureType.Rogue;
-                count = CreatureCount.Few;
-            }
-            else if (rand1 < 90) {
-                creature = CreatureType.Rogue;
-                count = CreatureCount.Several;
+            else if (rand < 90) {
+                _type = CreatureType.Halfling;
+                _count = CreatureCount.Few;
             }
             else {
-                creature = CreatureType.Rogue;
-                count = CreatureCount.Several;
+                _type = CreatureType.Halfling;
+                _count = CreatureCount.Pack;
             }
         }
-        
-        _creatureCode = (uint(count) * 100) + uint(creature);
     }
     
     
     // return all avaiable creatures (codes)
     function getCreatures() isPlayer public view returns(
-        uint _creatureCode0,
-        uint _creatureCode1,
-        uint _creatureCode2,
-        uint _creatureCode3,
-        uint _creatureCode4,
-        uint _creatureCode5
+        CreatureType _cType0,
+        CreatureCount _cCount0,
+        CreatureType _cType1,
+        CreatureCount _cCount1,
+        CreatureType _cType2,
+        CreatureCount _cCount2,
+        CreatureType _cType3,
+        CreatureCount _cCount3,
+        CreatureType _cType4,
+        CreatureCount _cCount4,
+        CreatureType _cType5,
+        CreatureCount _cCount5
     ){
-        address _addr = msg.sender;
-       _creatureCode0 = players[_addr].killedCreatures[0] < getDay(_addr) ? getCreature(_addr, 0) : 0;
-       _creatureCode1 = players[_addr].killedCreatures[1] < getDay(_addr) ? getCreature(_addr, 1) : 0;
-       _creatureCode2 = players[_addr].killedCreatures[2] < getDay(_addr) ? getCreature(_addr, 2) : 0;
-       _creatureCode3 = players[_addr].killedCreatures[3] < getDay(_addr) ? getCreature(_addr, 3) : 0;
-       _creatureCode4 = players[_addr].killedCreatures[4] < getDay(_addr) ? getCreature(_addr, 4) : 0;
-       _creatureCode5 = players[_addr].killedCreatures[5] < getDay(_addr) ? getCreature(_addr, 5) : 0;
+        uint today = getDay(msg.sender);
+        
+       (_cType0, _cCount0) = getCreature(msg.sender, today, 0);
+       (_cType1, _cCount1) = getCreature(msg.sender, today, 1);
+       (_cType2, _cCount2) = getCreature(msg.sender, today, 2);
+       (_cType3, _cCount3) = getCreature(msg.sender, today, 3);
+       (_cType4, _cCount4) = getCreature(msg.sender, today, 4);
+       (_cType5, _cCount5) = getCreature(msg.sender, today, 5);
     }
     
     function fightCreature(uint8 _creatureIdx) isPlayer public returns(
+        CreatureType _cType,
         uint _units,
-        uint _diamondsWon,
+        uint _healthLost,
+        uint _goldWon,
         uint _experienceWon
         ){
-        address _addr = msg.sender;
-         
+        require(!isDead(msg.sender));
+        
         // acepted creatures 0-6
         require(_creatureIdx < 6);
         
-        // check if this creature is killed today
-        require(players[_addr].killedCreatures[_creatureIdx] < getDay(_addr));
+  
+        uint today = getDay(msg.sender);
         
-        syncPlayer(_addr);
+        CreatureCount cCount;
         
-       
-        uint creatureCode = getCreature(_addr, _creatureIdx);
-        uint8 cType = uint8(creatureCode % 100);
-        CreatureCount cCount = CreatureCount((creatureCode - cType) / 100); // enum value (several, few...)
+        (_cType, cCount) = getCreature(msg.sender, today, _creatureIdx);
+        require(_cType != CreatureType.dead);
+
+        Creature memory creature = creatures[uint8(_cType)];
         
-        Creature memory creature = creatures[cType];
+        _units = creatureCount2Number(cCount);
         
-        _units = getCreaturesCount(cCount);
-        uint _totalHealth = creature.health * _units;
-        uint _totalDamage = random(generateSeed(), creature.damageMin, creature.damageMax) * _units;
-        //dummy code
-        uint _healthLost = 20;
+        uint playerHealth = getHealth(msg.sender);
+        
+        // creature health * units = total crreatures health
+        // multiply by 10000 for better decimal calculations, then devide by 10000;
+        // total crreatures health / player damage = how many attacks is needed to kill the creatures
+        // attakcs needed * creature damage = how many health player will lost
+        _healthLost = creature.health * _units * 10000 / getDamage(msg.sender) * creature.damage * _units / 10000; 
         
         // won
-        if (players[_addr].health > _healthLost) {
-            _diamondsWon = random(generateSeed(), creature.diamondsMin, creature.diamondsMax) * _units;
+        if (playerHealth > _healthLost) {
+            _goldWon = creature.gold * _units;
             _experienceWon = creature.experience * _units;
             
-            players[_addr].health -= _healthLost;
-            players[_addr].diamonds += _diamondsWon;
-            players[_addr].experience += _experienceWon;
+            players[msg.sender].health = playerHealth - _healthLost;
+            
+            players[msg.sender].gold += _goldWon;
+            players[msg.sender].experience += _experienceWon;
+            
+            syncLevel(msg.sender); // update users level after experience gain
             
             // mark creature as dead
-            players[_addr].killedCreatures[_creatureIdx] = getDay(_addr);
+            players[msg.sender].killedCreatures[_creatureIdx] = today;
         }
         else {
-            // ban player to play until next day
-            players[_addr].health = 0;
-            _diamondsWon = 0;
-            _experienceWon = 0;
-            
+             // players is dead for 1000 blocks (~4 h)
+            players[msg.sender].deadUntil = block.number + 1000;
+            players[msg.sender].health = getMaxHealth(msg.sender);
         }
-
+        
+        players[msg.sender].lastSynced = block.number;
     }
     
     function getPlayer() isPlayer public view returns(
         bytes12 _username,
+        uint _level,
         uint _day,
-        uint _diamonds,
+        uint _gold,
         uint _experience,
         uint _strength,
         uint _vitaility,
         uint _intelligence,
-        uint _health
+        uint _health,
+        uint _deadUntil
     ){
         address _addr = msg.sender;
         _username = players[_addr].username;
+        _level = players[_addr].level;
         _day = getDay(_addr);
-        _diamonds = players[_addr].diamonds;
+        _gold = players[_addr].gold;
         _experience = players[_addr].experience;
         _strength = getStrength(_addr);
         _vitaility = getVitaility(_addr);
         _intelligence = getIntelligence(_addr);
         _health = getHealth(_addr);
+        _deadUntil = players[_addr].deadUntil;
     }
 }
